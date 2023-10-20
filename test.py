28d5423354086934aa90ed5638a348107380ca23
Brady Langdale <bradylangdale@gmail.com>
2023-10-19 20:25:42 -0500

    Improve example code

--- test.py
+++ test.py
@@ -1,19 +1,43 @@
 import olympe
 import os
-import time
-from olympe.messages.ardrone3.Piloting import TakeOff, Landing
+from olympe.messages.ardrone3.Piloting import TakeOff, moveBy, Landing
+from olympe.messages.ardrone3.PilotingState import FlyingStateChanged
 
 DRONE_IP = os.environ.get("DRONE_IP", "10.202.0.1")
 
 
-def test_takeoff():
+def test_moveby():
     drone = olympe.Drone(DRONE_IP)
     drone.connect()
-    assert drone(TakeOff()).wait().success()
-    time.sleep(10)
+
+    assert drone(
+        TakeOff()
+        >> FlyingStateChanged(state="hovering", _timeout=5)
+    ).wait().success()
+
+    assert drone(
+        moveBy(2, 0, 0, 0)
+        >> FlyingStateChanged(state="hovering", _timeout=5)
+    ).wait().success()
+
+    assert drone(
+        moveBy(0, 0, 0, 3.14)
+        >> FlyingStateChanged(state="hovering", _timeout=5)
+    ).wait().success()
+
+    assert drone(
+        moveBy(2, 0, 0, 0)
+        >> FlyingStateChanged(state="hovering", _timeout=5)
+    ).wait().success()
+
+    assert drone(
+        moveBy(0, 0, 0, 3.14)
+        >> FlyingStateChanged(state="hovering", _timeout=5)
+    ).wait().success()
+
     assert drone(Landing()).wait().success()
     drone.disconnect()
 
 
 if __name__ == "__main__":
-    test_takeoff()
+    test_moveby()
