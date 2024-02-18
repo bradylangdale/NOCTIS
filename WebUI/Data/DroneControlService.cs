2cf310ece3020fb9be29ec26bce4d947abd053d0
Brady Langdale <bradylangdale@gmail.com>
2024-02-18 15:19:48 -0600

    Remove last hardcoded path and improve stream performance

--- WebUI/Data/DroneControlService.cs
+++ WebUI/Data/DroneControlService.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Text;
 using NetMQ;
 using NetMQ.Sockets;
+using System.Threading.Tasks.Dataflow;
 
 namespace WebUI.Data
 {
@@ -13,6 +14,8 @@ namespace WebUI.Data
         private bool running = true;
         private Queue<String> outQueue;
         private Queue<String> inQueue;
+
+        private string imagePath = "/Data/Videos/current_frame0.jpg" + "?DummyId=" + DateTime.Now.Ticks;
 
         public DroneControlService()
         {
@@ -27,7 +30,10 @@ namespace WebUI.Data
             thread = new Thread(() =>
             {
                 using (var client = new RequestSocket())
-                {
+                {   
+                    int display_num = 0;
+                    int write_num = 1;
+
                     client.Connect("tcp://localhost:5555");
                     while (running)
                     {
@@ -49,6 +55,36 @@ namespace WebUI.Data
                                     Console.WriteLine(e);
                                 }
                             }
+
+                            try {
+                                client.SendFrame("GetCurrentFrame");
+
+                                string str_count = write_num.ToString();
+                                str_count = (str_count == "") ? "0" : str_count; 
+
+                                System.IO.File.WriteAllBytes(
+                                    home + "/Projects/NOCTIS/WebUI/wwwroot/Data/Videos/current_frame" + str_count + ".jpg",
+                                    Encoding.GetEncoding("ISO-8859-1").GetBytes(client.ReceiveFrameString())
+                                );
+
+                                write_num += 1;
+                                write_num %= 32;
+
+                                str_count = display_num.ToString();
+
+                                str_count = (str_count == "") ? "0" : str_count;
+
+                                lock (imagePath)
+                                {
+                                    imagePath = "/Data/Videos/current_frame" + str_count + ".jpg" + "?DummyId=" + DateTime.Now.Ticks;
+                                }
+
+                                display_num += 1;
+                                display_num %= 32;
+
+                            } catch (Exception e) {
+                                Console.WriteLine(e);
+                            }
                         }
                     }
                 }
@@ -66,17 +102,12 @@ namespace WebUI.Data
             }
         }
 
-        public byte[] GetCurrentFrame()
+        public string GetCurrentFrame()
         {
-            lock (outQueue)
+            lock (imagePath)
             {
-                outQueue.Enqueue("GetCurrentFrame");
+                return imagePath;
             }
-
-            string rsp = "";
-            while(!CheckResponses(ref rsp));
-
-            return Encoding.GetEncoding("ISO-8859-1").GetBytes(rsp);
         }
 
         public bool CheckResponses(ref string response)
